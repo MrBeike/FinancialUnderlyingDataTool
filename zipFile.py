ae1d0ccaa747929523ad572dc263822e8d583b1b
xeroxyor <lbbas@126.com>
2020-12-27 22:19:21 +0800

    rebuild zipFile func

--- zipFile.py
+++ zipFile.py
@@ -1,44 +1,42 @@
 import os
+import zipfile
+
+# def workDirClean():
+#     fileList = os.listdir(workDir)
+#     for eachfile in fileList:
+#         try:
+#             suff = eachfile.split('.')[1]
+#             if suff in ['dat', 'log']:
+#                 os.remove(eachfile)
+#         except IndexError:
+#             pass
 
 
-def workDirClean():
-    fileList = os.listdir(workDir)
-    for eachfile in fileList:
-        try:
-            suff = eachfile.split('.')[1]
-            if suff in ['dat', 'log']:
-                os.remove(eachfile)
-        except IndexError:
-            pass
+# def getfileNames():
+#     '''
+#     获取工作文件夹下所有的xlsx格式文件
 
+#     '''
+#     fileList = os.listdir(workDir)
+#     excelFiles = []
+#     for eachfile in fileList:
+#         try:
+#             suff = eachfile.split('.')[1]
+#             if suff == 'xlsx':
+#                 excelFiles.append(eachfile)
+#         except IndexError:
+#             pass
+#     return excelFiles
 
-def getfileNames():
-    '''
-    获取工作文件夹下所有的xlsx格式文件
-
-    '''
-    fileList = os.listdir(workDir)
-    excelFiles = []
-    for eachfile in fileList:
-        try:
-            suff = eachfile.split('.')[1]
-            if suff == 'xlsx':
-                excelFiles.append(eachfile)
-        except IndexError:
-            pass
-    return excelFiles
-
-
-def zipFiles(fileName):
-    fileName_pre = fileName.split('.')[0]
-    zipFile = zipfile.ZipFile(os.path.join(
-        workDir, fileName_pre) + '.zip', 'w', zipfile.ZIP_DEFLATED)
-    f_txt = fileName_pre + '.dat'
-    f_log = fileName_pre + '.log'
+def zipFile(filename, path=''):
+    zipFilename = f'{os.path.join(path, filename)}.zip'
+    zipFile = zipfile.ZipFile(zipFilename, 'w', zipfile.ZIP_DEFLATED)
+    f_txt = filename + '.dat'
+    f_log = filename + '.log'
     try:
-        zipFile.write(os.path.join(workDir, f_txt), arcname=f_txt)
-        zipFile.write(os.path.join(workDir, f_log), arcname=f_log)
+        zipFile.write(os.path.join(path, f_txt), arcname=f_txt)
+        zipFile.write(os.path.join(path, f_log), arcname=f_log)
         zipFile.close()
-        print(f'{fileName}报表压缩包搞定啦！')
+        print(f'{filename}报表压缩包搞定啦！')
     except:
-        print(f'{fileName}报表dat文件或log文件丢失，请尝试重新运行本程序')
+        print(f'{filename}报表dat文件或log文件丢失，请尝试重新运行本程序')
