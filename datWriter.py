2fa294f6c3a11d18174ccdd441485bdd7b98ab81
xeroxYor <lbbas@126.com>
2020-12-28 13:01:53 +0800

    add idMask hook func

--- datWriter.py
+++ datWriter.py
@@ -8,14 +8,25 @@ def datWriter(filename, reportConfig, path=''):
     df = pd.read_excel(filename, header=0, sheet_name=0)
     columnNames = list(df)
     for key, value in reportConfig.items():
-        print(value)
-        # df[key] = df[key].map(lambda x: f'{x:.{value}f}')
         columnName = columnNames[key-1]
-        df[columnName] = df[columnName].map(lambda x: value(x))
+        # FIXME idMask函数补丁 如何区分各类证件？
+        if value.__name__ == 'idMask':
+            assistColumnName = columnNames[key-2]
+            assistDF = df[assistColumnName]
+            dataDF = df[columnName]
+            idRule = zip(assistDF,dataDF)
+            idMasked = [value(x,y) for x,y in idRule]
+            df[columnName] = idMasked
+        else:
+            df[columnName] = df[columnName].map(lambda x: value(x))
     content = df.to_csv(None, header=False, index=False,
                         sep='|', encoding='utf-8')
     # to_csv会自动多空一行，手动去除后两行写入文件
     datFilename = os.path.join(path, filename.replace('xlsx', 'dat'))
+    # 清理已存在的同名dat文件，保证创建时间准确性
+    existsFlag = os.path.exists(datFilename)
+    if existsFlag:
+        os.remove(datFilename)
     with open(datFilename, 'w', newline='', encoding='utf-8') as f:
         f.write(content[:-2])
 
